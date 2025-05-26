c385644d4f7a9cae63644832605908251211a369
Todd Chapman <todd.chapman@energyq.com.au>
2025-05-27 08:11:32 +1000

    comment update

--- 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
+++ 02-Terraform-Basics/02-02-Terraform-Command-Basics/terraform-manifests/ec2-instance.tf
@@ -19,3 +19,6 @@ resource "aws_instance" "ec2demo" {
   ami           = "ami-0be2609ba883822ec" # Amazon Linux in us-east-1, update as per your region
   instance_type = "t2.micro"
 }
+#Comment test
+
+
